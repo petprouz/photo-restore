dd5c4a3b340249d66b5cb68e36ea708fbbd53695
sherdor <admin@example.com>
2025-06-05 16:03:35 +0500

    Add health check endpoint

--- backend/restore/urls.py
+++ backend/restore/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health/', health_check, name='health_check'),
     path('api/', include('photo_restore.urls')),
     path('api/', include('users.urls')),
 ]
